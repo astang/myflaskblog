6d6fb5733721b6f3e5a47b6720c67ed16eb45925
Alice Stang <alice@stacom.de>
2018-12-26 22:23:34 +0900

    added static folder for css file and updated css reference in myflaskblog.py and layout.html

--- myflaskblog.py
+++ myflaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 #__name__ is a special variable in python that is just the name of the module.
 #if we run the script with python directly its equal to __main__
 #that flask knows where to look for templates and static files etc.
@@ -27,7 +27,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About page!')
 
 #if you want to run it directly from python in debug mode without using the global 
 if __name__=='__main__':
