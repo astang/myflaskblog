06bbf6ca34124864b280cc75ae78bcb4a05c0e06
Alice Stang <alice@stacom.de>
2018-12-26 21:26:08 +0900

    added comments to initial code

--- myflaskblog.py
+++ myflaskblog.py
@@ -1,6 +1,10 @@
 from flask import Flask
+#__name__ is a special variable in python that is just the name of the module.
+#if we run the script with python directly its equal to __main__
+#that flask knows where to look for templates and static files etc.
 app = Flask(__name__)
 
+#define the routes
 @app.route("/")
 @app.route("/home")
 def home():
@@ -10,5 +14,6 @@ def home():
 def about():
     return "<h1>About Page!</h1>"
 
+#if you want to run it directly from python in debug mode without using the global 
 if __name__=='__main__':
     app.run(debug=True)
