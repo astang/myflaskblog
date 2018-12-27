a27d2b927023492acbb59c95cbd591cf03f517ae
Alice Stang <alice@stacom.de>
2018-12-27 14:33:26 +0900

    myflaskblog.py changed from wtfforms to wtforms and added content for register.html

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 #Flasi extensions to write python code which will be converted to html (WebDeveloper are writing Forms in html)
 from flask_wtf import FlaskForm
-from wtfforms import StringField, PasswordField, SubmitField, BooleanField
-from wtfforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
