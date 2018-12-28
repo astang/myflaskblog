361c94c8dfbba3b93cbdb87caf0373c415c4688c
Alice Stang <alice@stacom.de>
2018-12-28 22:24:39 +0900

    added layout logout funtionality

--- myflaskblog/routes.py
+++ myflaskblog/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, url_for, flash, redirect
 from myflaskblog import app, db, bcrypt
 from myflaskblog.forms import RegistrationForm, LoginForm
 from myflaskblog.models import User, Post
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -32,6 +32,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -48,6 +50,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -57,3 +61,9 @@ def login():
         else:
             flash('Login unsuccessful. Please check email and password.', 'danger')
     return render_template('login.html', title='Login', form=form) 
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
