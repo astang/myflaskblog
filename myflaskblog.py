1edd24992b7fbe4c924026d4845e8cfbd977cfa1
Alice Stang <alice@stacom.de>
2018-12-28 19:02:44 +0900

    imported models and __main__ db just as a preparation

--- myflaskblog.py
+++ myflaskblog.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from models import User, Post
 
 #__name__ is a special variable in python that is just the name of the module.
 #if we run the script with python directly its equal to __main__
@@ -13,30 +14,6 @@ app.config['SECRET_KEY']='0048eb592aa1faf4bc89e5ca99a68a85'
 app.config['SQLAlchemy_DATABASE_URI'] ='sqlite:///site.db'
 #create a SQL Instance
 db = SQLAlchemy(app)
-
-class User(db.Model) : 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    #one to many relationship one author cna have many post, one post depends on one author
-    #referencing the class Post
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model) : 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    #refrencing the table namen and column name not the class thats why user small case
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date}')"
 
 posts = [
     {
