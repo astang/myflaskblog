ed0fcbe2bd443aa977185962258e0cfb30f85637
Alice Stang <alice@stacom.de>
2018-12-29 13:51:50 +0900

    added UpdateAccountForm

--- myflaskblog/forms.py
+++ myflaskblog/forms.py
@@ -1,5 +1,6 @@
 #Flasi extensions to write python code which will be converted to html (WebDeveloper are writing Forms in html)
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from myflaskblog.models import User
@@ -20,10 +21,24 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('email already exists. Please choose a different one.')
 
-
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign Up')
     
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('User name already exists. Please choose a different one.')
+    def validate_email(self, email):
+        if username.email != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email already exists. Please choose a different one.')
