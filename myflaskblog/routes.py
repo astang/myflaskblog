672c788301cbd5c7f5f16e559c1b32c47418ba62
Alice Stang <alice@stacom.de>
2019-01-02 17:08:27 +0900

    added post delete function

--- myflaskblog/routes.py
+++ myflaskblog/routes.py
@@ -117,7 +117,7 @@ def new_post():
         db.session.commit()
         flash('Your Post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form) 
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post') 
     
 @app.route("/post/<int:post_id>")
 def post(post_id):
@@ -125,12 +125,34 @@ def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        #db.add not needed cause post is already in db
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect( url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        #filling form with current post data
+        form.title.data = post.title
+        form.content.data = post.content
         #403 is a http abort import for forbidden route
-    return render_template('create_post.html', title='Update Post', form=form) 
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post') 
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
