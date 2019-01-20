872d5ae187d11b0a23c1793906c709371fa85e20
Alice Stang <alice@stacom.de>
2019-01-20 16:33:27 +0100

    enviroment variable working! (restart terminal fixed the issue)

--- myflaskblog/__init__.py
+++ myflaskblog/__init__.py
@@ -23,10 +23,8 @@ login_manager.login_message_category='info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] =587
 app.config['MAIL_USE_TLS'] =True
-#app.config['MAIL_USERNAME'] =os.environ.get('EMAIL_USER')
-#app.config['MAIL_PASSWORD'] =os.environ.get('EMAIL_PASS')
-app.config['MAIL_USERNAME'] ='blogtest2019@gmail.com'
-app.config['MAIL_PASSWORD'] ='20test19'
+app.config['MAIL_USERNAME'] =os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] =os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
