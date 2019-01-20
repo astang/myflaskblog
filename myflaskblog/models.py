e9d3cb9bc768aef43bc4ad7af478074fb178647d
Alice Stang <alice@stacom.de>
2019-01-20 08:59:16 +0100

    added expire token

--- myflaskblog/models.py
+++ myflaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from myflaskblog import db, login_manager #from __init__py not from __main__
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from myflaskblog import db, login_manager, app #from __init__py not from __main__
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -15,6 +16,18 @@ class User(db.Model, UserMixin) :
     #one to many relationship one author cna have many post, one post depends on one author
     #referencing the class Post
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
